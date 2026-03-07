de4f3b86b52f680db591791e08917b1593eb01d7
KRITHIKSAI16 <krishakstudios@gmail.com>
2026-03-07 13:54:09 +0530

    implement and test jwt authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const { protect } = require("./middleware/authMiddleware");
+
