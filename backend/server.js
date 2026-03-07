d186b02df8f4f4649f9bc8521aefd4b6139582c0
KRITHIKSAI16 <krishakstudios@gmail.com>
2026-03-07 13:41:56 +0530

    resolved the db problem

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+// CONNECT DATABASE
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Basic Test Route
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Smart Onboarding API Running...");
 });
@@ -19,7 +27,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/api/auth", authRoutes);
