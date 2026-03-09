1c3d940d16689dcee97516e2f52ab6468398c54e
KRITHIKSAI16 <krishakstudios@gmail.com>
2026-03-09 18:03:40 +0530

    add role based authorization middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -32,4 +32,17 @@ const protect = (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const authorize = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access denied: insufficient permissions"
+      });
+    }
+
+    next();
+  };
+};
+
+module.exports = { protect ,authorize };
